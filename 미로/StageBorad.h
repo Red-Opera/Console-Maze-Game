be7820aafc8c39e21f0203cef37f89960ee5e071
RedZoo <hyunsim01@naver.com>
2025-01-27 05:47:38 +0900

    파일 올리기

---
+++ 미로/StageBorad.h
@@ -0,0 +1,236 @@
+﻿/* 이 해더 파일은 미로판의 구조와 각 스테이지 별 지형 정의,이동의 정의 ,각 값에 따라서 출력하는 상태를 표현합니다.
+    0:빈공간 1:벽 2:플레이어 3:목적지 */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#define GAME_STAGE1 stange1()
+#define GAME_STAGE2 stange2()
+#define GAME_STAGE3 stange3()
+#define GAME_STAGE4 stange4()
+#define Print print()
+#define WIN win()
+#define STAGECLRER stageClear()
+
+#define GOUP up()
+#define GODOWN down()
+#define GOLEFT left()
+#define GORIGHT right()
+#define CLEAR system("cls");
+
+int playerX = 0;
+int playerY = 0;
+
+struct gameBoradSetting
+{
+	char gameBorad[25][79];
+}gameBoradSetting;
+
+void stange1() {
+	for (int j = 0; j < 3; j++) {
+		for (int i = 0; i < 79; i++) {
+			if ((j == 0) || (j == 2) || ((j == 1) && (i == 0)) || ((j == 1) && (i == 78))) {
+				gameBoradSetting.gameBorad[j][i] = 1;
+			}
+		}
+	}
+	gameBoradSetting.gameBorad[1][1] = 2;
+	gameBoradSetting.gameBorad[1][77] = 3;
+	playerX = 1;
+	playerY = 1;
+}
+
+void stange2() {
+	for (int j = 0; j < 25; j++) {
+		for (int i = 0; i < 79; i++) {
+			if (((j == 24) || (j == 0) || (i == 8) || (i == 0) || (i == 78) || ((i % 4 == 0) && (j != 23)) || ((i % 4 == 2) && (j != 1))) && i < 9) {
+				gameBoradSetting.gameBorad[j][i] = 1;
+			}
+			else {
+				gameBoradSetting.gameBorad[j][i] = 0;
+			}
+		}
+	}
+	gameBoradSetting.gameBorad[23][1] = 2;
+	gameBoradSetting.gameBorad[23][7] = 3;
+	playerX = 1;
+	playerY = 23;
+}
+
+void stange3() {
+	for (int j = 0; j < 16; j++) {
+		for (int i = 0; i < 16; i++) {
+			if ((i == 0) || (i == 15) || (j == 15) || (j == 0) || ((i == 1) && ((j == 9) || (j == 11) || (j == 12) || (j == 13))) || ((i == 2) && ((j == 2) || (j == 3) || (j == 4) || (j == 5) || (j == 7) || (j == 8))) || ((i == 3) && ((j == 5) || (j == 9) || (j == 10) || (j == 11) || (j == 12) || (j == 13))) || ((i == 4) && ((j == 2) || (j == 3) || (j == 5))) || ((i == 5) && ((j == 2) || (j == 5) || (j == 6) || (j == 7) || (j == 9) || (j == 10) || (j == 11))) || ((i == 6) && ((j == 1) || (j == 2) || (j == 5) || (j == 7) || (j == 11) || (j == 12) || (j == 13) || (j == 14))) || ((i == 7) && ((j == 2) || (j == 3) || (j == 4) || (j == 5) || (j == 7) || (j == 8) || (j == 9) || (j == 11))) || ((i == 8) && ((j == 7) || (j == 12) || (j == 13))) || ((i == 9) && ((j == 1) || (j == 2) || (j == 3) || (j == 5) || (j == 6) || (j == 7) || (j == 10) || (j == 13))) || ((i == 10) && ((j == 1) || (j == 9) || (j == 10) || (j == 13))) || ((i == 11) && ((j == 1) || (j == 3) || (j == 4) || (j == 5) || (j == 8) || (j == 9) || (j == 12) || (j == 13))) || ((i == 12) && ((j == 2) || (j == 3) || (j == 5) || (j == 7))) || ((i == 13) && ((j == 1) || (j == 5) || (j == 6) || (j == 7) || (j == 8) || (j == 9) || (j == 10) || (j == 11) || (j == 12) || (j == 14))) || ((i == 14) && ((j == 3)))) {
+				gameBoradSetting.gameBorad[j][i] = 1;
+			}
+			else {
+				gameBoradSetting.gameBorad[j][i] = 0;
+			}
+		}
+	}
+	gameBoradSetting.gameBorad[14][1] = 2;
+	gameBoradSetting.gameBorad[1][14] = 3;
+	playerX = 1;
+	playerY = 14;
+}
+
+void stange4() {
+}
+
+void stange5() {
+
+}
+
+void stange6() {
+
+}
+
+void print() {
+	HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
+	for (int j = 0; j < 25; j++) {
+		for (int i = 0; i < 79; i++) {
+			if (gameBoradSetting.gameBorad[j][i] == 1)
+				printf("#");
+			else if (gameBoradSetting.gameBorad[j][i] == 2) {
+				SetConsoleTextAttribute(color,10);
+				printf("P");
+				SetConsoleTextAttribute(color, 7);
+			}
+			else if (gameBoradSetting.gameBorad[j][i] == 3) {
+				SetConsoleTextAttribute(color, 14);
+				printf("*");
+				SetConsoleTextAttribute(color, 7);
+			}
+			else {
+				if ((j != 24) && (i != 78)) {
+					printf(" ");
+				}
+			}
+			if ((i == 78) && (gameBoradSetting.gameBorad[j][0] != NULL)) {
+				if ((j != 24) || (i != 78)) {
+					printf("\n");
+				}
+			}
+		}
+	}
+}
+
+void left() {
+	if (gameBoradSetting.gameBorad[playerY][playerX - 1] != 1) {
+		gameBoradSetting.gameBorad[playerY][playerX] = 0;
+		gameBoradSetting.gameBorad[playerY][playerX - 1] = 2;
+		playerX = playerX - 1;
+	}
+}
+
+void right() {
+	if (gameBoradSetting.gameBorad[playerY][playerX + 1] != 1) {
+		gameBoradSetting.gameBorad[playerY][playerX] = 0;
+		gameBoradSetting.gameBorad[playerY][playerX + 1] = 2;
+		playerX = playerX + 1;
+	}
+}
+
+void up() {
+	if (gameBoradSetting.gameBorad[playerY-1][playerX] != 1) {
+		gameBoradSetting.gameBorad[playerY][playerX] = 0;
+		gameBoradSetting.gameBorad[playerY-1][playerX] = 2;
+		playerY = playerY - 1;
+	}
+}
+
+void down() {
+	if (gameBoradSetting.gameBorad[playerY+1][playerX] != 1) {
+		gameBoradSetting.gameBorad[playerY][playerX] = 0;
+		gameBoradSetting.gameBorad[playerY + 1][playerX] = 2;
+		playerY = playerY + 1;
+	}
+}
+
+void win() {
+	CLEAR;
+	HANDLE color = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	for (int i = 0; i < 5; i++) {
+		SetConsoleTextAttribute(color, 9);
+		printf("        *                        *                                  *          \n");
+		printf("          *     *         *                         *                     *    \n");
+		printf("    *                                                               *          \n");
+		printf("            *                        *          *                              \n");
+		printf("                      *                                               *        \n");
+		printf("                           *               *               *                   \n");
+		printf("       *          *                                                *       *   \n");
+		printf("               *                                 *                             \n");
+		printf("   *                 ■■■  ■  ■■■      ■      ■■■    *     *         \n");
+		printf("        *   *        ■      ■  ■         ■■     ■  ■            *  *    \n");
+		printf("                *    ■      ■  ■■■    ■  ■    ■■■        *           \n");
+		printf("            *        ■      ■  ■       ■■■■   ■   ■   *          *    \n");
+		printf("      *              ■■■  ■  ■■■  ■      ■  ■    ■          *    *  \n");
+		printf("             *                        *                       *        *       \n");
+		printf("          *      *             *                                  *            \n");
+		printf("       *                                        *          *               *   \n");
+		printf("   *       *       *                *        *        *          *     *       \n");
+		printf("                                                                               \n");
+		printf("         *     *         *     *        *                          *           \n");
+		printf("                                               *     *         *          *    \n");
+		printf("         *           *         *                                               \n");
+		printf("   *        *                         *         *       *       *   *      *   \n");
+		printf("        *              *      *              *              *      *           \n");
+		printf("     *      *       *       *       *           *             *                \n");
+		printf("    *         *      *       *          *                               *      \n");
+		Sleep(1000);
+		CLEAR;
+		SetConsoleTextAttribute(color, 12);
+		printf("        *                        *                                  *          \n");
+		printf("          *     *         *                         *                     *    \n");
+		printf("    *                                                               *          \n");
+		printf("            *                        *          *                              \n");
+		printf("                      *                                               *        \n");
+		printf("                           *               *               *                   \n");
+		printf("       *          *                                                *       *   \n");
+		printf("               *                                 *                             \n");
+		printf("   *                 ■■■  ■  ■■■      ■      ■■■    *     *         \n");
+		printf("        *   *        ■      ■  ■         ■■     ■  ■            *  *    \n");
+		printf("                *    ■      ■  ■■■    ■  ■    ■■■        *           \n");
+		printf("            *        ■      ■  ■       ■■■■   ■   ■   *          *    \n");
+		printf("      *              ■■■  ■  ■■■  ■      ■  ■    ■          *    *  \n");
+		printf("             *                        *                       *        *       \n");
+		printf("          *      *             *                                  *            \n");
+		printf("       *                                        *          *               *   \n");
+		printf("   *       *       *                *        *        *          *     *       \n");
+		printf("                                                                               \n");
+		printf("         *     *         *     *        *                          *           \n");
+		printf("                                               *     *         *          *    \n");
+		printf("         *           *         *                                               \n");
+		printf("   *        *                         *         *       *       *   *      *   \n");
+		printf("        *              *      *              *              *      *           \n");
+		printf("     *      *       *       *       *           *             *                \n");
+		printf("    *         *      *       *          *                               *      \n");
+		Sleep(1000);
+		CLEAR;
+	}
+}
+
+void stageClear() {
+	for (int j = 0; j < 25; j++) {
+		for (int i = 0; i < 79; i++) {
+			if ((j != 24) && (i != 78)) {
+				gameBoradSetting.gameBorad[j][i] = 0;
+			}
+		}
+	}
+}
+
+void removeCursor()
+
+{
+
+	CONSOLE_CURSOR_INFO cur;
+
+	GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cur);
+
+	cur.bVisible = 0;
+
+	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cur);
+
+}
---
+++ 미로/미로.cpp
@@ -0,0 +1,269 @@
+﻿#pragma warning(disable:4996)
+#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <time.h>
+#include <conio.h>
+#include <mmsystem.h>
+#include "StageBorad.h"
+#pragma comment(lib,"winmm")
+#define UP 72
+#define DOWN 80
+#define LEFT 75
+#define RIGHT 77
+#define SPACE 10
+#define ENTER 13
+#define Y 89
+#define y 118
+#define N 78
+#define n 100
+#define CLEAR system("cls");
+
+int Select = 1;
+char stage = 0;
+
+void gameMain() {
+	system("mode con cols=80 lines=25");
+	printf("    ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃            ■■■■■         ■■■■■■■■             ┃    \n");
+	printf("    ┃            ■       ■  ■                    ■           ┃    \n");
+	printf("    ┃            ■       ■  ■                    ■           ┃    \n");
+	printf("    ┃            ■       ■  ■    ■■■■■■■■             ┃    \n");
+	printf("    ┃            ■       ■  ■    ■                           ┃    \n");
+	printf("    ┃            ■       ■  ■    ■                           ┃    \n");
+	printf("    ┃            ■       ■  ■    ■■■■■■■■             ┃    \n");
+	printf("    ┃            ■       ■  ■                                 ┃    \n");
+	printf("    ┃            ■       ■  ■            ■                   ┃    \n");
+	printf("    ┃            ■       ■  ■            ■                   ┃    \n");
+	printf("    ┃            ■■■■■   ■    ■■■■■■■■             ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                            새로운 시작 ");
+	if (Select == 1) printf(" ≪");
+	else printf("    ");
+    printf("	         ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                               계속  ");
+	if (Select == 2) printf("    ≪");
+	else printf("    ");
+	printf("    	 	 ┃    \n");
+
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┃                              끝내기  ");
+	if (Select == 3) printf("   ≪");
+	else printf("     ");
+	printf("   확인 : Enter  ┃    \n");
+	printf("    ┃                                                            ┃    \n");
+	printf("    ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛   ");
+}
+
+void startPrint();
+
+void nextStage1();
+
+void nextStage2();
+
+void nextStage3();
+
+int main() {
+
+	system("title 미로");
+
+	removeCursor();
+
+	FILE *onfile = fopen("MGS.GSV", "rb");
+
+	int Choose = 0, reGame = 0;
+
+
+	fscanf(onfile, "%d", &stage);
+	fclose(onfile);
+
+	gameMain();
+
+	while (Choose != 13) {
+
+		Choose = getch();
+
+		switch (Choose) {
+
+		case 72:
+			if (Select > 1) { //올라가기
+				CLEAR;
+				Select--;
+				gameMain();
+			}
+			break;
+		case 80:
+			if (Select < 3) { // 내려가기
+				CLEAR;
+				Select++;
+				gameMain();
+			}
+			break;
+		default:  
+			break;
+		}
+	}
+
+	if (Select == 1) { // 처음으로
+		stage = 49;
+	reset:
+		nextStage1();
+		onfile = fopen("MGS.GSV", "wb");
+		nextStage2();
+		fprintf(onfile, "%d", stage);
+		fclose(onfile);
+		onfile = fopen("MGS.GSV", "wb");
+		nextStage3();
+		fclose(onfile);
+	}
+	else if (Select == 2) {  // 계속
+		CLEAR;
+		if (onfile == NULL || feof(onfile) == EOF || stage == 0) {  // 파일이 없는 경우
+			printf("저장된 파일이 없습니다.");
+			Sleep(3000);
+			CLEAR;
+			printf("새로 시작 하시겠습니까? Y/N");
+			while ((reGame != 89) && (reGame != 118) && (reGame != 78) && (reGame != 100)) {
+				reGame = getch();
+
+				switch (reGame)
+				{
+				case 89:        // Y
+					goto reset;
+				case 118:       // y
+					goto reset;
+				case 78:        // N
+					exit(0);
+				case 100:       // n
+					exit(0);
+				default:
+					break;
+
+				}
+			}
+
+		}
+	}
+	else if (Select == 3) {  // 끝내기 할 경우
+		exit(0);
+	}
+	else {
+		system("title ERROR");
+		system("COLOR c");
+		CLEAR;
+		printf("ERROR");
+		Sleep(4000);
+		exit(0);
+	} // 선택오류
+
+	system("pause");
+
+}
+
+void startPrint() {
+	int startGame = 0, starX, starY;
+
+	Print;
+	do {
+		startGame = getch();
+
+		switch (startGame) {
+		case 72:
+			GOUP;
+			CLEAR;
+			break;
+		case 80:
+			GODOWN;
+			break;
+		case 75:
+			GOLEFT;
+			break;
+		case 77:
+			GORIGHT;
+			break;
+		}
+		
+		for (int j = 0; j < 25; j++) {
+			for (int i = 0; i < 79; i++) {
+				if (gameBoradSetting.gameBorad[j][i] == 3) {
+					starX = i;
+					starY = j;
+					CLEAR;
+					Print;
+					goto starend;
+				}
+				else if (gameBoradSetting.gameBorad[j][i] != 3) {
+					starX = NULL;
+					starY = NULL;
+				}
+			}
+		}
+		
+	starend:
+		if (starX == NULL) {
+			CLEAR;
+			PlaySound(TEXT("Victory.wav"), NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+			WIN;
+			system("COLOR 7");
+		}
+		PlaySound(0, NULL, NULL);
+	} while (starX != NULL);
+}
+
+void gameBoardClear() {
+	for (int j = 0; j < 25; j++) {
+		for (int i = 0; i < 79; i++)
+			gameBoradSetting.gameBorad[j][i] = 0;
+	}
+}
+
+void nextStage1() {
+	CLEAR;
+	printf("\n\n\n\n\n\n\n\n\n             ■■■      ■■■      ■        ■■■      ■■■     ■\n");
+	printf("             ■            ■       ■■       ■          ■         ■\n");
+	printf("             ■■■        ■      ■  ■      ■■■      ■■■     ■\n");
+	printf("                 ■        ■     ■■■■     ■  ■      ■         ■\n");
+	printf("             ■■■        ■    ■      ■    ■■■      ■■■     ■\n");
+	Sleep(3000);
+	gameBoardClear();
+	GAME_STAGE1;
+	CLEAR;
+	startPrint();
+}
+
+void nextStage2() {
+	
+	CLEAR;
+	printf("\n\n\n\n\n\n\n\n\n             ■■■      ■■■      ■        ■■■      ■■■     ■■■\n");
+	printf("             ■            ■       ■■       ■          ■             ■\n");
+	printf("             ■■■        ■      ■  ■      ■■■      ■■■     ■■■\n");
+	printf("                 ■        ■     ■■■■     ■  ■      ■         ■\n");
+	printf("             ■■■        ■    ■      ■    ■■■      ■■■     ■■■\n");
+	Sleep(3000);
+	gameBoardClear();
+	GAME_STAGE2;
+	stage++;
+	CLEAR;
+	startPrint();
+}
+
+void nextStage3() {
+	CLEAR;
+	printf("\n\n\n\n\n\n\n\n\n             ■■■      ■■■      ■        ■■■      ■■■     ■■■\n");
+	printf("             ■            ■       ■■       ■          ■             ■\n");
+	printf("             ■■■        ■      ■  ■      ■■■      ■■■     ■■■\n");
+	printf("                 ■        ■     ■■■■     ■  ■      ■             ■\n");
+	printf("             ■■■        ■    ■      ■    ■■■      ■■■     ■■■\n");
+	Sleep(3000);
+	gameBoardClear();
+	GAME_STAGE3;
+	stage++;
+	CLEAR;
+	startPrint();
+}
